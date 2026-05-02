c6ac2df714e0d3fe28c03d4803981b5c2d424930
Samuel Stuart <sasdallas765@gmail.com>
2026-05-02 15:15:00 -0500

    Add tinycc patch

---
+++ tinycc/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if ! [ -n "$SYSROOT" ]; then
+	echo "The \$SYSROOT environ must be set to build this port"
+	exit 1
+fi
+
+if ! [ -n "$TRIPLET" ]; then
+	echo "The \$TRIPLET environ must be set to build this port"
+	exit 1
+fi
+
+set -e
+mkdir output || true
+git clone https://github.com/TinyCC/tinycc
+cd tinycc
+
+git apply ../patches/0001-Add_ethereal_support.patch
+# thanks to tayoky for figuring out the configs
+./configure --targetos=ethereal --enable-static --sysroot=$SYSROOT --prefix=/usr --cc=$CC --triplet=$TRIPLET --elfinterp=/usr/lib/ld.so --libpaths=/usr/lib:/usr/lib/tcc --sysincludepaths=/usr/include:/usr/lib/tcc/include/ --crtprefix=/usr/lib
+
+make XTCC=gcc XAR=ar all
+make DESTDIR=$SYSROOT install
+cd ../../
